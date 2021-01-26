6dd27847b74526e5b5a866b2bffaffe04f6cf34b
zhoujj <nujnujouhz@foxmail.com>
2021-01-26 17:54:45 +0800

    [add] add port create function

--- include/ports.go
+++ include/ports.go
@@ -41,7 +41,7 @@ type PtEntry struct {
 
 	PtState  uint16 // port state: free, limbo, alloc
 	PtMaxCnt uint16 // max messages to be queued
-	PtSeq    int32  // sequence change at creation
+	PtSeq    int32  // sequence change at creation. much like the sequence number of TCP
 
 	PtHead *MsgNode // head of message list
 	PtTail *MsgNode // tail of message list
@@ -51,7 +51,7 @@ type PtEntry struct {
 var PortTab [MaxPorts]PtEntry
 
 // PtNextID is the next table entry to try in PortTab
-var PtNextID int32
+var PtNextID int
 
 // ptfree is the head of global list of free message node
 var ptfree *MsgNode
@@ -87,4 +87,42 @@ func PtInit(maxmsgs int32) error {
 	curr.PtNext = nil
 
 	return OK
+}
+
+// PtCreate function create a port that allows 'count' outstanding messages
+func PtCreate(count uint16) (int, error) {
+	mask := Disable()
+	defer Restore(mask)
+
+	if count <= 0 {
+		return -1, ErrSYSERR
+	}
+
+	for i := 0; i < MaxPorts; i++ {
+		ptnum := PtNextID // try allocate this port entry
+
+		// update PtNextID for next try
+		PtNextID++
+		if PtNextID >= MaxPorts {
+			PtNextID = 0
+		}
+
+		ptptr := &PortTab[ptnum]
+		if ptptr.PtState == PtStateFree { // free port entry can allocate
+			ptptr.PtState = PtStateAlloc // update state
+
+			ptptr.PtRsem, _ = SemCreate(0)            // cannot receive message right now
+			ptptr.PtSsem, _ = SemCreate(int32(count)) // can send message count times
+
+			ptptr.PtHead = nil
+			ptptr.PtTail = nil
+
+			ptptr.PtSeq++
+			ptptr.PtMaxCnt = count
+
+			return ptnum, OK
+		}
+	}
+
+	return -1, ErrEMPTY
 }
